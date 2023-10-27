5531d84f98e735a3008d7470096b9bfdd890394b
Summer <snrobinson13@gmail.com>
2023-10-27 14:00:23 -0700

    clean up code

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -41,8 +41,6 @@ const Form = () => {
 					Create Account
 				</button>
 			</FormBlock>
-
-			<div className=""></div>
 		</form>
 	);
 };
--- src/components/FormBlock.jsx
+++ src/components/FormBlock.jsx
@@ -1,5 +1,3 @@
-import InputBlock from "./InputBlock";
-
 const FormBlock = ({ children }) => {
 	return <div className=" flex flex-col md:flex-row md:-mx-2 ">{children}</div>;
 };
